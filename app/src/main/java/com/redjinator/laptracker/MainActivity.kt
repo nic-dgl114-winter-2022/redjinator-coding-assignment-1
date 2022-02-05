0fce44e3e20cc254f977295b04fb5e8b1bab55b3
Redjinator <71242881+Redjinator@users.noreply.github.com>
2022-02-04 22:59:53 -0800

    Add: start lap function to start lap button

--- app/src/main/java/com/redjinator/laptracker/MainActivity.kt
+++ app/src/main/java/com/redjinator/laptracker/MainActivity.kt
@@ -2,10 +2,26 @@ package com.redjinator.laptracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private var begin   : Long = 0
+    private var end     : Long = 0
+
+    // onCreate
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Get start button and add listener
+        val startButton : Button = findViewById(R.id.start_button)
+        startButton.setOnClickListener {
+            startLap()
+        }
     }
+
+    // Start Lap function
+    fun startLap() { begin = System.currentTimeMillis() }
+
 }
